155ee0a3bf0aae8bb561429e4a1f54776b55d8a1
0x1306a94 <0x1306a94@gmail.com>
2024-09-17 19:43:04 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # ibackup_trim
+
+#### 创建备份文件
+
+```bash
+idevicebackup2 backup --full -u <udid> <backup_path>
+```
+
+#### 创建影子数据
+* 会在`out`目录下创建对应的软链
+```bash
+python main.py --shadow --backup <backup_path> --out <out_path>
+```
+* 后续操作中的 `backup` 路径均为影子数据路径
+
+#### 列出App
+
+```bash
+python main.py --list-app --backup <backup_path>
+```
+
+#### 删除App
+
+```bash
+python main.py --delete-app --backup <backup_path> --app <app_name>
+```
+
+#### 列出Domain
+
+```bash
+python main.py --list-domain --backup <backup_path>
+```
+
+#### 删除Domain
+
+```bash
+python main.py --delete-domain --backup <backup_path> --domain <domain_name>
+```
+
+#### 恢复备份
+
+```bash
+idevicebackup2 restore --system --remove -d <shadow_path>
+```
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 import argparse
+import shutil
 from biplist import *
 import plistlib
 
@@ -127,17 +128,70 @@ WHERE
     c.execute(sql)
     conn.commit()
 
+def shadow(backup, out):
+    # 获取backup目录的最后一级名称
+    backup_name = os.path.basename(os.path.normpath(backup))
+    
+    # 将out目录与backup最后一级名称结合
+    out = os.path.join(out, backup_name)
+
+    # 确保输出目录存在
+    os.makedirs(out, exist_ok=True)
+
+    # 将backup和out转换为绝对路径
+    backup = os.path.abspath(backup)
+    out = os.path.abspath(out)
+
+    # 遍历backup目录中的所有文件和子目录
+    for root, dirs, files in os.walk(backup):
+        for file in files:
+            # 获取源文件的完整路径
+            src_path = os.path.abspath(os.path.join(root, file))
+            # 计算相对路径
+            rel_path = os.path.relpath(src_path, backup)
+            # 构建目标路径
+            dst_path = os.path.abspath(os.path.join(out, rel_path))
+
+            # 确保目标目录存在
+            os.makedirs(os.path.dirname(dst_path), exist_ok=True)
+
+            # 检查文件名是否为特殊文件
+            special_files = ['Info.plist', 'Manifest.db', 'Manifest.plist', 'Status.plist']
+            if file in special_files:
+                # 对于特殊文件，复制而不是创建软链接
+                shutil.copy2(src_path, dst_path)
+            else:
+                # 创建软链接
+                try:
+                    os.symlink(src_path, dst_path)
+                except FileExistsError:
+                    # 如果目标已存在，则先删除再创建软链接
+                    os.remove(dst_path)
+                    os.symlink(src_path, dst_path)
+
+    print(f"已在 {out} 目录中创建 {backup} 目录的软链接和复制特殊文件")
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='ibackup_trim')
     parser.add_argument('--backup', help='备份文件目录', required=True)
+    parser.add_argument('--out', help='输出目录')
     group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--shadow', action='store_true', help='启用影子模式')
     group.add_argument('--list-app', action='store_true', help='列出App')
     group.add_argument('--list-domain', action='store_true', help='列出Domain')
     group.add_argument('--delete-app', help='删除指定app')
     group.add_argument('--delete-domain', help='删除指定Domain')
     args = parser.parse_args()
     
+    if args.shadow:
+        if not args.out:
+            parser.error("使用 --shadow 时必须指定 --out 参数")
+        else:
+            shadow(backup=args.backup, out=args.out)
+            exit(0)
+    
     conn = sqlite3.connect(os.path.join(args.backup, 'Manifest.db'))
+   
     if args.list_app:
         listApp(conn=conn)
     elif args.list_domain:
